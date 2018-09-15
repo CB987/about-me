4dff54a4dedb834cd8700de0c46ab11d30ddc186
Clare Barton <grimalkinclare@gmail.com>
2018-09-15 18:24:23 -0400

    inserting new pic

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ var myImage = document.querySelector("img");
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute("src");
     if(mySrc === "images/sky.jpg") {
-        myImage.setAttribute ("src", "images/sunrise-sky.jpg")
+        myImage.setAttribute ("src", "images/fam-in-chicago.jpg")
     } else {
         myImage.setAttribute ("src", "images/sky.jpg");
     }
